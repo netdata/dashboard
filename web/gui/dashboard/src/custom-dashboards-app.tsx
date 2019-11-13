b50f6623ead540499e7e99898a78a1a76243d6de
Jacek Kolasa <jacek.kolasa@gmail.com>
2019-11-13 14:08:37 +0100

    separate task for main.js building. changes did to main.js to allow adding it as a module and run basic functionalities of Dashboard

---
+++ web/gui/dashboard/src/custom-dashboards-app.tsx
@@ -0,0 +1,33 @@
+import React from "react"
+import Ps from "perfect-scrollbar"
+
+import { loadCss } from "utils/css-loader"
+import "domains/chart/utils/jquery-loader"
+import { Portals } from "domains/chart/components/portals"
+import "./types/global"
+
+if (!window.netdataNoBootstrap) {
+  // it needs to be imported indirectly, there's probably a bug in webpack
+  import("dynamic-imports/bootstrap")
+}
+
+if (!window.netdataNoFontAwesome) {
+  // @ts-ignore
+  import("vendor/fontawesome-all-5.0.1.min")
+}
+
+// support legacy code
+window.Ps = Ps
+
+loadCss(window.NETDATA.themes.current.bootstrap_css)
+loadCss(window.NETDATA.themes.current.dashboard_css)
+
+const CustomDashboardsApp: React.FC = () => { // eslint-disable-line arrow-body-style
+  return (
+    <div className="App">
+      <Portals />
+    </div>
+  )
+}
+
+export default CustomDashboardsApp
--- web/gui/dashboard/src/index.tsx
+++ web/gui/dashboard/src/index.tsx
@@ -1,21 +1,39 @@
-import React from "react"
+import React, { lazy, Suspense } from "react"
 import ReactDOM from "react-dom"
 import { Provider } from "react-redux"
 
 import { configureStore } from "store"
 
 import "./index.css"
-import App from "./App"
 import * as serviceWorker from "./serviceWorker"
+
+const CustomDashboardsApp = lazy(() => import("./custom-dashboards-app"))
+const App = lazy(() => import("./App"))
 
 const store = configureStore()
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root"),
-)
+if (process.env.REACT_APP_IS_MAIN_DASHBOARD) {
+// Dashboard
+  ReactDOM.render(
+    <Provider store={store}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <App />
+      </Suspense>
+    </Provider>,
+    document.getElementById("root"),
+  )
+} else {
+// Custom Dashboards
+  ReactDOM.render(
+    <Provider store={store}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <CustomDashboardsApp />
+      </Suspense>
+    </Provider>,
+    document.getElementById("root"),
+  )
+}
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
