055e594a6526b65a6fcd8034a9f55621f05e8e50
Jacek Kolasa <jacek.kolasa@gmail.com>
2019-11-13 13:51:48 +0100

    refresh portals when the component updates (needed for main.js)

--- web/gui/dashboard/src/domains/chart/components/portals.tsx
+++ web/gui/dashboard/src/domains/chart/components/portals.tsx
@@ -4,23 +4,26 @@ import { createPortal } from "react-dom"
 import { getAttributes } from "../utils/transformDataAttributes"
 import { ChartContainer } from "./chart-container"
 
-const nodesArray = Array.from(document.querySelectorAll("[data-netdata-react]"))
+const getNodesArray = () => Array.from(document.querySelectorAll("[data-netdata]"))
 
-export const Portals = () => (
-  <>
-    {nodesArray.map((node, index) => {
-      const attributesMapped = getAttributes(node) as { id: string }
-      return (
-        createPortal(
-          <ChartContainer
-            attributes={getAttributes(node)}
-            // todo change to uuid generator (when we disconnect dashboard.js)
-            chartUuid={`${attributesMapped.id}-${index}`}
-            portalNode={(node as HTMLElement)}
-          />,
-          node,
+export const Portals = () => {
+  const nodes = getNodesArray()
+  return (
+    <>
+      {nodes.map((node, index) => {
+        const attributesMapped = getAttributes(node) as { id: string }
+        return (
+          createPortal(
+            <ChartContainer
+              attributes={getAttributes(node)}
+              // todo change to uuid generator (when we disconnect dashboard.js)
+              chartUuid={`${attributesMapped.id}-${index}`}
+              portalNode={(node as HTMLElement)}
+            />,
+            node,
+          )
         )
-      )
-    })}
-  </>
-)
+      })}
+    </>
+  )
+}
