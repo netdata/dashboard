241069caef5c917388eb955d4b0d24b1f236d664
Jacek Kolasa <jacek.kolasa@gmail.com>
2019-11-15 12:47:11 +0100

    use server detection to make api/v1/data and api/v1/chart calls

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,6 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:19999/api/v1/",
   // timeout: 30 * 1000, // todo
   headers: {
     "Cache-Control": "no-cache, no-store",
