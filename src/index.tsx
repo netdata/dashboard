70fb3a1fb4b98d7b965a89d43df41d2f0c8c12de
Jacek Kolasa <jacek.kolasa@gmail.com>
2019-11-22 21:12:53 +0100

    remove service worker

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,6 @@ import { configureStore } from "store"
 
 import "./index.css"
 import App from "./App"
-import * as serviceWorker from "./serviceWorker"
 
 const store = configureStore()
 
@@ -16,8 +15,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root"),
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
