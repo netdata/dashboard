69c7d588b6169c7ceae4e31b77e418e1cc37413a
Jacek Kolasa <jacek.kolasa@gmail.com>
2020-01-15 22:09:09 +0100

    7729: parse url and detect print mode

---
+++ src/domains/dashboard/utils/parse-url.ts
@@ -0,0 +1,51 @@
+import {
+  mergeAll, pipe, split, mergeRight,
+} from "ramda"
+import { mapIndexed } from "ramda-adjunct"
+
+const defaultUrlOptions = {
+  hash: "#",
+  theme: null,
+  help: null,
+  mode: "live", // 'live', 'print'
+  update_always: false,
+  pan_and_zoom: false,
+  server: null,
+  after: 0,
+  before: 0,
+  highlight: false,
+  highlight_after: 0,
+  highlight_before: 0,
+  nowelcome: false,
+  show_alarms: false,
+  chart: null,
+  family: null,
+  alarm: null,
+  alarm_unique_id: 0,
+  alarm_id: 0,
+  alarm_event_id: 0,
+  alarm_when: 0,
+} as {[key: string]: unknown}
+
+const parseQueryPair = (queryTuple: [string, string]): {[key: string] : unknown} => {
+  const [key, value] = queryTuple
+  if (defaultUrlOptions[key] === undefined || value === undefined) {
+    return {}
+  }
+  return {
+    [key]: decodeURIComponent(value),
+  }
+}
+
+export const parseUrl = pipe(
+  split(";"),
+  mapIndexed((value, index) => (
+    (index === 0) ? { hash: value } : parseQueryPair((value.split("=") as [string, string]))
+  )),
+  mergeAll,
+  mergeRight(defaultUrlOptions),
+)
+
+const urlParsed = parseUrl(document.location.hash)
+
+export const isPrintMode = urlParsed.mode === "print"
