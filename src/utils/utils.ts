072970bc6fdc68d8e77c7530b01e57f932de27f8
Jacek Kolasa <jacek.kolasa@gmail.com>
2020-01-30 14:08:08 +0100

    7766: use naming const instead of 1000 (re CR)

---
+++ src/utils/utils.ts
@@ -0,0 +1,6 @@
+// we use numbers to specify time. it can be either a timestamp (ms), or a relative value in seconds
+// which is always 0 or less (0 is now, -300 is -5 minutes)
+
+export const isTimestamp = (x: number) => x > 0
+
+export const MS_IN_SECOND = 1000
