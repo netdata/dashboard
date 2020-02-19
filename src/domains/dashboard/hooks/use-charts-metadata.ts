0400699ded46f39af12763a5a85355b75381aec1
Jacek Kolasa <jacek.kolasa@gmail.com>
2020-02-19 11:06:55 +0100

    p14: use-http and use-charts-metadata hooks

---
+++ src/domains/dashboard/hooks/use-charts-metadata.ts
@@ -0,0 +1,8 @@
+import { useHtpp } from "hooks/use-http"
+import { serverDefault } from "utils/server-detection"
+import { ChartsMetadata } from "domains/global/types"
+
+export const useChartsMetadata = () => {
+  const [chartsMetadata] = useHtpp<ChartsMetadata>(`${serverDefault}api/v1/charts`)
+  return chartsMetadata
+}
