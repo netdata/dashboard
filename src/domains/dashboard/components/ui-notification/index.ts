f442a66e99280811c7fff4f15642f6836ebfe52d
VLegakis <vlegakis@gmail.com>
2020-03-30 22:58:11 +0300

    Remove unused Notifications components

--- src/domains/dashboard/components/ui-notification/index.ts
+++
@@ -1,6 +0,0 @@
-export { UINotification, createUINotification } from "./ui-notification"
-export {
-  ErrorNotification,
-  createErrorNotification,
-  createRetryActionControl,
-} from "./error-notification"
