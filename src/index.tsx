6d6035a94c3c56ef30ea1e006b6b9d2bac87528d
Jacek Kolasa <jacek.kolasa@gmail.com>
2020-05-24 22:17:29 +0200

    load App.tsx synchronously with the initial bundle (speeds up initial load a little bit), by aliasing it with name from env variable

--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,22 @@
-import React, { lazy, Suspense } from "react"
+import React from "react"
 import ReactDOM from "react-dom"
 import { Provider } from "react-redux"
 
 import { store } from "store"
-import { isMainJs } from "utils/env"
+
+// resolved in craco.config
+import App from "App"
 
 import "./index.css"
 
-const CustomDashboardsApp = lazy(() => import("./custom-dashboards-app"))
-const App = lazy(() => import("./App"))
+// todo for static-dashboard:
+// 1) wait for the whole page to load, then render
+// 2) when the whole page is loaded, check window.NETDATA.options set by user and override initial
+//    options settings
 
-if (isMainJs) {
-// Dashboard
-  ReactDOM.render(
-    <Provider store={store}>
-      <Suspense fallback={<div>Loading...</div>}>
-        <App />
-      </Suspense>
-    </Provider>,
-    document.getElementById("root"),
-  )
-} else {
-// Custom Dashboards
-  ReactDOM.render(
-    <Provider store={store}>
-      <Suspense fallback={<div>Loading...</div>}>
-        <CustomDashboardsApp />
-      </Suspense>
-    </Provider>,
-    document.getElementById("root"),
-  )
-}
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root"),
+)
