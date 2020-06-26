8b8bd673975a42330fb4a6bdbb452bacf8784395
Jacek Kolasa <jacek.kolasa@gmail.com>
2020-06-26 13:44:47 +0200

    pass attributes and metadata to dropdown callback (needed by custom-dashboards)

--- src/domains/chart/components/chart-dropdown/chart-dropdown.tsx
+++ src/domains/chart/components/chart-dropdown/chart-dropdown.tsx
@@ -1,23 +1,36 @@
 import React, { useState, ReactNode } from "react"
 
+import { Attributes } from "domains/chart/utils/transformDataAttributes"
+import { ChartMetadata } from "domains/chart/chart-types"
+
 import { List, SimpleListItem } from "@rmwc/list"
 import { MenuSurface, MenuSurfaceAnchor } from "@rmwc/menu"
 
 import * as S from "./styled"
 
+interface DropdownMenuCallbackProps {
+  attributes: Attributes,
+  chartMetadata: ChartMetadata,
+  chartID: string,
+}
+
 export type DropdownMenu = {
   icon: ReactNode,
   label: string,
-  onClick: (chartID: string) => void,
+  onClick: (dropdownMenuCallbackProps: DropdownMenuCallbackProps) => void,
 }[]
 
 interface Props {
+  attributes: Attributes
   chartID: string
+  chartMetadata: ChartMetadata
   dropdownMenu: DropdownMenu
 }
 
 export const ChartDropdown = ({
+  attributes,
   chartID,
+  chartMetadata,
   dropdownMenu,
 }: Props) => {
   const [isOpen, setIsOpen] = useState(false)
@@ -49,7 +62,7 @@ export const ChartDropdown = ({
                   </S.DropdownItem>
                 )}
                 onClick={() => {
-                  onClick(chartID)
+                  onClick({ attributes, chartMetadata, chartID })
                   handleClose()
                 }}
               />
