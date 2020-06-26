8769df04b9a539946735f163245d41d11394c010
Jacek Kolasa <jacek.kolasa@gmail.com>
2020-06-26 17:58:57 +0200

    export additional types to be used in cloud-fe, bump version

--- src/index-npm.ts
+++ src/index-npm.ts
@@ -15,12 +15,7 @@ import "./styles/bootstrap-3.3.7.css"
 
 import "vendor/fontawesome-all-5.0.1.min"
 
-// Cannot re-export a type when the --isolatedModules flag is provided
-import { Attributes } from "domains/chart/utils/transformDataAttributes"
-
 window.Ps = Ps
-
-export type ChartAttributes = Attributes
 
 export {
   dashboardReduxContext,
@@ -40,3 +35,17 @@ export { selectStopUpdatesWhenFocusIsLost, selectDestroyOnHide } from "domains/g
 export { STOP_UPDATES_WHEN_FOCUS_IS_LOST, DESTROY_ON_HIDE } from "domains/global/options"
 
 export { VersionControl } from "components/app-header/components/version-control"
+
+/**
+ types
+**/
+
+// Cannot re-export a type when the --isolatedModules flag is provided
+import { Attributes } from "domains/chart/utils/transformDataAttributes"
+export type ChartAttributes = Attributes
+
+import { ChartMetadata as ChartMetadata_ } from "domains/chart/chart-types"
+export type ChartMetadata = ChartMetadata_
+
+import { ChartsMetadata as ChartsMetadata_ } from "domains/global/types"
+export type ChartsMetadata = ChartsMetadata_
