c1e5cfd56535ef0ae6ba57c69f5c085dab7e3235
Jacek Kolasa <jacek.kolasa@gmail.com>
2020-09-09 17:07:53 +0200

    p1404: move "Privacy policy" & "Terms and conditions" links from sidebar to the bottom of the dashboard

--- src/main.js
+++ src/main.js
@@ -1908,11 +1908,6 @@ function renderPage(menus, data) {
 
     sidebar += '<br/>&nbsp;<br/><strong>netdata</strong><br/>' + data.version.toString() + '</small>';
 
-    sidebar += '<div class="terms-and-privacy">'
-        sidebar += '<a href="https://www.netdata.cloud/terms/" target="_blank">Terms and conditions</a><br/>'
-        sidebar += '<a href="https://www.netdata.cloud/privacy/" target="_blank">Privacy Policy</a><br/>'
-    sidebar += '</div>'
-
     sidebar += '</li>'
 
     sidebar += '<li id="sidebar-end-portal-container"></li>'
