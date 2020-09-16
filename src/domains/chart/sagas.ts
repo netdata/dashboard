7ae087c331f0d06b1369abe146917b5c82bec552
novykh <ioannis.klironomos@gmail.com>
2020-09-16 15:03:44 +0300

    Added context param.

--- src/domains/chart/sagas.ts
+++ src/domains/chart/sagas.ts
@@ -133,6 +133,8 @@ function* fetchDataSaga({ payload }: Action<FetchDataPayload>) {
     after,
     before,
     dimensions,
+    context: chart,
+    aggr_method: "avg",
   }
 
   const onSuccessCallback = (data: {}) => {
