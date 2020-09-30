e1bb4c4970924247712fc5f3be2820b7ed870dea
Jacek Kolasa <jacek.kolasa@gmail.com>
2020-09-30 23:47:10 +0200

    support additional, different /data call format (with POST)

--- src/utils/netdata-sdk/metrics-stream.ts
+++ src/utils/netdata-sdk/metrics-stream.ts
@@ -4,7 +4,7 @@ import {
 import {
   mergeMap, tap, catchError, startWith, switchMap,
 } from "rxjs/operators"
-import { CancelTokenSource } from "axios"
+import { CancelTokenSource, Method } from "axios"
 
 import { UnknownStringKeyT } from "types/common"
 
@@ -14,8 +14,10 @@ export const CHART_UNMOUNTED = "Chart scrolled out of view"
 
 interface FetchInputEvent {
   url: string
-  params: UnknownStringKeyT
-  onErrorCallback: (data?: { [key: string]: unknown }) => void
+  method?: Method
+  params?: UnknownStringKeyT
+  data?: UnknownStringKeyT
+  onErrorCallback: (error: Error) => void
   onSuccessCallback: (data: { [key: string]: unknown }) => void
   cancelTokenSource?: CancelTokenSource
 }
@@ -27,10 +29,15 @@ export const getFetchStream = (concurrentCallsLimit: number) => {
   const resetFetch$ = new Subject()
 
   const handler = mergeMap(({
-    url, params, onErrorCallback, onSuccessCallback, cancelTokenSource,
+    url, method = "GET", params = {}, data, onErrorCallback, onSuccessCallback, cancelTokenSource,
   }: FetchInputEvent) => (
-    from(axiosInstance.get(url, {
-      params, timeout: METRICS_TIMEOUT, cancelToken: cancelTokenSource?.token,
+    from(axiosInstance.request({
+      url,
+      method,
+      params,
+      data,
+      timeout: METRICS_TIMEOUT,
+      cancelToken: cancelTokenSource?.token,
     })).pipe(
       tap(({ data }) => { onSuccessCallback(data) }),
       catchError((error) => {
