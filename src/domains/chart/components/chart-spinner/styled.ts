1bed805394b4264176d54cdf534627200c31e340
Jacek Kolasa <jacek.kolasa@gmail.com>
2020-10-07 10:58:54 +0200

    new animated svg spinner

--- src/domains/chart/components/chart-spinner/styled.ts
+++ src/domains/chart/components/chart-spinner/styled.ts
@@ -3,46 +3,30 @@ import styled, { keyframes } from "styled-components"
 
 import { getColor } from "@netdata/netdata-ui"
 
-const spinnerAnimation = keyframes`
-  to {
-    transform: rotate(360deg);
+const svgSpinnerAnimation = keyframes`
+  0% {
+   transform: translate(10px,10px) rotate(0deg) translate(-10px,-10px);
+  }
+  100% {
+   transform: translate(10px,10px) rotate(350deg) translate(-10px,-10px);
   }
 `
 
-export const GifSpinnerContainer = styled.div<{ margin: number }>`
+export const GifSpinnerContainer = styled.div<{ top: number, right: number }>`
   position: absolute;
-  top: ${prop("margin")}px;
-  right: ${prop("margin")}px;
+  top: ${prop("top")}px;
+  right: ${prop("right")}px;
+  opacity: .5;
 `
 
-const clockRadius = 8
-const clockLine = 1
-const clockColor = "border"
+export const SvgSpinner = styled.g`
+  animation: 1s linear infinite both ${svgSpinnerAnimation};
+`
 
+export const SvgSpinnerPath = styled.path`
+  fill: ${getColor("border")};
+`
 
-export const ChartSpinner = styled.div<{ margin: number }>`
-  position: absolute;
-  top: ${prop("margin")}px;
-  right: ${prop("margin")}px;
-  
-  width: ${clockRadius * 2}px;
-  height: ${clockRadius * 2}px;
-  background-color: rgba(0, 0, 0, 0);
-  box-shadow: inset 0 0 0 ${clockLine}px ${getColor(clockColor)};
-  border-radius: 50%;
-  
-  &:after {
-    position: absolute;
-    content: "";
-    transform-origin: ${clockLine / 2}px ${clockLine / 2}px;
-    background-color: ${getColor(clockColor)};
-    height: ${clockLine}px;
-    top: ${clockRadius - clockLine / 2}px;
-    left: ${clockRadius - clockLine / 2}px;
-  }
-  
-  &:after {
-    width: ${clockRadius * (5 / 6)}px;
-    animation: ${spinnerAnimation} 0.6s linear infinite;
-  }
+export const SvgCircle = styled.path`
+  fill: ${getColor("border")};
 `
