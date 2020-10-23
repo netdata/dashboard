677587799283fc30d0f053d95e5fbf702b92269f
Jacek Kolasa <jacek.kolasa@gmail.com>
2020-10-23 22:16:28 +0200

    header icons + its styling fixes

--- src/components/app-header/components/version-control/icons-list.ts
+++
@@ -1,7 +0,0 @@
-import updateCloudIcon from "./update-cloud-icon.svg"
-import noUpdateCloudIcon from "./no-update-cloud-icon.svg"
-
-export const iconsList = {
-  updateCloudIcon,
-  noUpdateCloudIcon,
-}
--- src/components/app-header/components/version-control/styled.ts
+++ src/components/app-header/components/version-control/styled.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components"
 import {
-  getSizeBy, getColor, Text, breakpoints,
+  getColor, Text, breakpoints,
 } from "@netdata/netdata-ui"
 
 export const Container = styled.div`
@@ -10,7 +10,6 @@ export const Container = styled.div`
   display: flex;
   flex-flow: row nowrap;
   align-items: center;
-  margin-right: ${getSizeBy(3)};
 
   @media ${breakpoints.mobileSmall} {
     display: none;
@@ -34,19 +33,14 @@ export const TextBold = styled(Text)`
   }
 `
 
-export const VersionIndicator = styled.div`
-  display: flex;
-  flex-flow: row nowrap;
-  align-items: center;
-`
-
-const VersionIndicatorLink = VersionIndicator.withComponent("a")
-
-export const NewVersionIndicator = styled(VersionIndicatorLink)`
-  margin-left: ${getSizeBy(2)};
-  color: ${getColor("primary")};
-  cursor: pointer;
-
+export const NewVersionIndicator = styled.div`
+  flex-shrink: 0;
+  flex-grow: 0;
+  position: relative;
+  margin-right: -26px; // make up unnecessary fixed button width
+  .button-icon {
+    fill: ${getColor("accent")};
+  }
   &:hover {
     color: ${getColor("bright")};
 
@@ -54,11 +48,6 @@ export const NewVersionIndicator = styled(VersionIndicatorLink)`
       color: ${getColor("bright")};
     }
   }
-`
-
-export const VersionNumber = styled(Text)`
-  margin-left: 4px;
-  color: ${getColor("bright")};
 `
 
 export const CollapsableText = styled(Text)`
@@ -69,8 +58,4 @@ export const CollapsableText = styled(Text)`
   @media ${breakpoints.desktop} {
     display: block;
   }
-`
-
-export const NoUpdateCloud = styled.a`
-  cursor: pointer;
 `
