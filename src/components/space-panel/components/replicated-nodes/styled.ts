9972f18b5f18147f0cbc7310ac2b9f4ec4765504
Jacek Kolasa <jacek.kolasa@gmail.com>
2020-10-30 21:28:03 +0100

    fix space-panel colors in dark theme (dont change based on theme, yet)

--- src/components/space-panel/components/replicated-nodes/styled.ts
+++ src/components/space-panel/components/replicated-nodes/styled.ts
@@ -44,7 +44,7 @@ export const StyledIcon = styled(Icon)`
   flex-shrink: 0;
   flex-grow: 0;
   margin-right: ${getSizeBy(2)};
-  fill: ${getColor("text")};
+  fill: ${getColor(["gray", "arsenic"])};
 `
 
 export const MasterNodeContainer = styled.div`
