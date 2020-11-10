5ea5e3814322e099ad77e56fa427c878de13f094
Jacek Kolasa <jacek.kolasa@gmail.com>
2020-11-10 21:19:02 +0100

    z-indexes needed for date-picker

--- src/styles/z-index.ts
+++ src/styles/z-index.ts
@@ -7,3 +7,8 @@ export const spacesBarZIndex = 8
 export const spacePanelZIndex = 6
 
 export const appHeaderZIndex = 5
+
+// the same as in cloud
+export const portalSidebarZIndex = "z-index: 35;"
+export const customDropdownZIndex = "z-index: 45;"
+export const dialogsZIndex = 60
