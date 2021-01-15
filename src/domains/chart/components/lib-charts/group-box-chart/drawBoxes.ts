cb61ef97df49166e9284052bc8bf3f05d104bed5
allelos <p.paliagkas@gmail.com>
2021-01-15 15:23:48 +0200

    Updated to draw based on defined aspect ratio

--- src/domains/chart/components/lib-charts/group-box-chart/drawBoxes.ts
+++ src/domains/chart/components/lib-charts/group-box-chart/drawBoxes.ts
@@ -1,12 +1,13 @@
 // @ts-nocheck
 import { scaleLog, extent, select } from "d3"
 
-const numOfColumns = 32
 const cellSize = 13.5
+const aspectRatio = Math.round(16 / 9)
 
-const getRows = (data) => Math.ceil(data.length / numOfColumns)
+const getRows = (data) => Math.sqrt(data.length / aspectRatio)
+const getColumns = (rows) => rows * aspectRatio
 
-const getXPosition = () => {
+const getXPosition = (numOfColumns) => {
   let col = 1
   return (_, index) => {
     if (col && !(index % numOfColumns)) col = 1
@@ -15,25 +16,25 @@ const getXPosition = () => {
   }
 }
 
-const getYPosition = (_, index) => Math.floor(index / numOfColumns) * cellSize
+const getYPosition = (numOfColumns) => (_, index) => Math.floor(index / numOfColumns) * cellSize
 
-const getCanvasSize = (data) => {
+const getCanvasAttributes = (data) => {
   const numOfRows = getRows(data)
-  const width = numOfRows === 1 ? data.length * cellSize : numOfColumns * cellSize
-  const height = numOfRows * cellSize + cellSize
-  return { width, height }
+  const numOfColumns = getColumns(numOfRows)
+  const width = Math.ceil(numOfColumns) * cellSize
+  const height = Math.ceil(numOfRows) * cellSize + cellSize
+  return { width, height, columns: Math.ceil(numOfColumns) }
 }
 
-const colorScale = (values) =>
-  scaleLog()
-    .domain(extent(values, (value) => value))
-    .range(["#C6E3F6", "#0E9AFF"])
+const colorScale = (values) => scaleLog()
+  .domain(extent(values, (value) => value))
+  .range(["#C6E3F6", "#0E9AFF"])
 
 export default (el, { onMouseover, onMouseout }) => {
   const svg = select(el)
 
   const update = ({ data }) => {
-    const { width, height } = getCanvasSize(data)
+    const { width, height, columns } = getCanvasAttributes(data)
     svg.attr("width", width)
     svg.attr("height", height)
 
@@ -47,19 +48,29 @@ export default (el, { onMouseover, onMouseout }) => {
       .merge(enter)
       .attr("rx", 2)
       .attr("ry", 2)
-      .attr("x", getXPosition())
-      .attr("y", getYPosition)
+      .attr("x", getXPosition(columns))
+      .attr("y", getYPosition(columns))
       .attr("width", cellSize - 1.5)
       .attr("height", cellSize - 1.5)
       .style("opacity", 0.9)
       .style("fill", (value) => colorScale(data)(value))
       .on("mouseover", (value, index, boxes) => {
         select(boxes[index]).style("stroke", "#DCE2E8")
-        onMouseover({ value, index, target: boxes[index], boxes })
+        onMouseover({
+          value,
+          index,
+          target: boxes[index],
+          boxes,
+        })
       })
       .on("mouseout", (value, index, boxes) => {
         select(boxes[index]).style("stroke", "")
-        onMouseout({ value, index, target: boxes[index], boxes })
+        onMouseout({
+          value,
+          index,
+          target: boxes[index],
+          boxes,
+        })
       })
 
     updated.exit().remove()
