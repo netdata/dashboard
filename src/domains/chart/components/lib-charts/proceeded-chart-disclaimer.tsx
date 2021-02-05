2c6985faaea548ec49114f91769b8a6e0e1e23c2
Jacek Kolasa <jacek.kolasa@gmail.com>
2021-02-05 20:34:54 +0100

    stop proceed-chart-disclaimer from reaching too far right + small redesign

--- src/domains/chart/components/lib-charts/proceeded-chart-disclaimer.tsx
+++ src/domains/chart/components/lib-charts/proceeded-chart-disclaimer.tsx
@@ -1,27 +1,39 @@
 import React, { forwardRef } from "react"
 import styled from "styled-components"
+import classNames from "classnames"
 
 const Container = styled.div`
-  display: block;
+  display: block !important; // ìmportant to fix styled-components race condition
 `
 
-const ProceededChartDisclaimer = forwardRef((
-  props: React.HTMLAttributes<HTMLElement>,
-  ref: React.Ref<HTMLDivElement>,
-) => (
-  <Container ref={ref} className="dygraph__history-tip" data-testid="proceededChartDisclaimer">
-    <span className="dygraph__history-tip-content">
-      Want to extend your history of real-time metrics?
-      <br />
-      <a href="https://docs.netdata.cloud/docs/configuration-guide/#increase-the-metrics-retention-period" target="_blank" rel="noopener noreferrer" data-testid="proceededChartDisclaimer-configure">
-        Configure Netdata&apos;s&nbsp;
-        <b>history</b>
-      </a>
-      &nbsp;or use the&nbsp;
-      <a href="https://docs.netdata.cloud/database/engine/" target="_blank" rel="noopener noreferrer" data-testid="proceededChartDisclaimer-engine">DB engine</a>
-      .
-    </span>
-  </Container>
-))
+interface Props {
+  centerHorizontally?: boolean
+}
+const ProceededChartDisclaimer = forwardRef(
+  ({ centerHorizontally }: Props, ref: React.Ref<HTMLDivElement>) => (
+    <Container
+      ref={ref}
+      className={classNames("dygraph__history-tip", {
+        "dygraph__history-tip--centered": centerHorizontally,
+      })}
+      data-testid="proceededChartDisclaimer"
+    >
+      <span className="dygraph__history-tip-content">
+        <span className="dygraph__history-tip-header">Want to see more historical metrics?</span>
+        <br />
+        Configure Netdata&apos;s
+        <br />
+        <a
+          href="https://docs.netdata.cloud/docs/configuration-guide/#increase-the-metrics-retention-period"
+          target="_blank"
+          rel="noopener noreferrer"
+          data-testid="proceededChartDisclaimer-configure"
+        >
+          time-series database.
+        </a>
+      </span>
+    </Container>
+  ),
+)
 
 export default ProceededChartDisclaimer
--- src/domains/chart/hooks/use-proceeded-chart.ts
+++ src/domains/chart/hooks/use-proceeded-chart.ts
@@ -1,6 +1,8 @@
 import React, { useRef } from "react"
 import { useToggle } from "react-use"
 
+const elementWidth = 230 + 50 // 50 is twice the margin-right
+
 const useProceededChart = (firstEntry: number):
   [boolean, React.Ref<HTMLElement>, (g: Dygraph) => void] => {
   const [proceeded, toggleProceeded] = useToggle(false)
@@ -8,14 +10,19 @@ const useProceededChart = (firstEntry: number):
   const ref = useRef<HTMLElement>(null)
 
   const updatePosition = (g: Dygraph) => {
-    const { x } = g.getArea()
+    const { x, w } = g.getArea()
     const distance = g.toDomXCoord(firstEntry * 1000)
     const hasProceeded = distance > x
     toggleProceeded(hasProceeded)
 
     if (hasProceeded && ref.current) {
+      // if user scrolls to the past too much, leave element in the middle
+      const rightFixed = (w - elementWidth) / 2
+      const rightGlued = w + x - distance
+
+      const right = Math.max(rightFixed, rightGlued)
       ref.current.style.left = `${x}px`
-      ref.current.style.right = `calc(100% - ${distance}px)`
+      ref.current.style.right = `${right}px`
     }
   }
 
