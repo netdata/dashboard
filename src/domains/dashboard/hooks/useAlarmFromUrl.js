483db482afbacc1714c57d722e1b7462de2f8e96
Jacek Kolasa <jacek.kolasa@gmail.com>
2021-03-12 20:38:26 +0100

    paint area in dygraph when alarm is provided via url

---
+++ src/domains/dashboard/hooks/useAlarmFromUrl.js
@@ -0,0 +1,35 @@
+import { useMount } from "react-use"
+
+import { useDispatch } from "store/redux-separate-context"
+import { getHashParams } from "utils/hash-utils"
+import { setAlarmAction, setGlobalPanAndZoomAction } from "domains/global/actions"
+
+export default () => {
+  const dispatch = useDispatch()
+  useMount(() => {
+    const params = getHashParams()
+    const alarmWhen = params["alarm_when"]
+    if (alarmWhen) {
+      const alarmTime = Number(alarmWhen)
+
+      const alarmStatus = params["alarm_status"]
+      const alarmChart = params["alarm_chart"]
+      const alarmValue = params["alarm_value"]
+
+      dispatch(setAlarmAction({
+        alarm: {
+          chart: alarmChart || "system.load",
+          // @ts-ignore
+          status: alarmStatus,
+        value: alarmValue,
+        when: alarmTime,
+        },
+      }))
+      const PADDING = 1000 * 60 * 5
+      dispatch(setGlobalPanAndZoomAction({
+        after: alarmTime * 1000 - PADDING,
+        before: alarmTime * 1000 + PADDING,
+      }))
+    }
+  })
+}
