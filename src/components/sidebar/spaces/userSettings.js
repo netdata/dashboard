a4403775b9201d36e844ad8677e18c1fd385f38d
allelos <p.paliagkas@gmail.com>
2021-03-24 19:16:20 +0200

    Refactored sidebar component.

---
+++ src/components/sidebar/spaces/userSettings.js
@@ -0,0 +1,33 @@
+import React from "react"
+import { ThemeProvider } from "styled-components"
+import { Flex, Menu, Icon, DarkTheme } from "@netdata/netdata-ui"
+import { getIframeSrc } from "@/src/utils"
+
+const menuItems = [{ children: "Sign Out" }]
+
+const UserSettings = () => {
+  return (
+    <ThemeProvider theme={DarkTheme}>
+      <Menu
+        icon={<Icon name="user" size="small" color="text" />}
+        caret={false}
+        items={menuItems}
+        padding={[2]}
+        renderDropdown={() => (
+          <Flex margin={[5, 18]} column width={52} background="mainBackground" padding={[3]} round>
+            <Flex
+              alignItems="center"
+              as="iframe"
+              src={getIframeSrc("https://staging.netdata.cloud", "sign-out")}
+              border={{ side: "all", size: "0px" }}
+              height="24px"
+            />
+          </Flex>
+        )}
+        dropProps={{ align: { top: "top", right: "right" } }}
+      />
+    </ThemeProvider>
+  )
+}
+
+export default UserSettings
