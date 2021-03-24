1753a9ae292ad6cb75aca92ad955de303a712f36
allelos <p.paliagkas@gmail.com>
2021-03-24 19:24:52 +0200

    Split up authentication iframes and sign in into different components.

---
+++ src/components/sidebar/signIn/index.js
@@ -0,0 +1,38 @@
+import React, { useCallback, useMemo } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import styled from "styled-components"
+import { Flex, Button } from "@netdata/netdata-ui"
+import { showSignInModalAction } from "domains/dashboard/actions"
+import { selectSignInUrl, selectRegistry } from "domains/global/selectors"
+import { NETDATA_REGISTRY_SERVER } from "utils/utils"
+
+const SignIn = ({ offline }) => {
+  const signInUrl = useSelector(selectSignInUrl)
+  const registry = useSelector(selectRegistry)
+
+  const dispatch = useDispatch()
+
+  const link = useMemo(() => {
+    const { href } = window.location
+    const redirectURI = encodeURIComponent(href)
+    return `${signInUrl}&redirect_uri=${redirectURI}`
+  }, [signInUrl])
+
+  const onSignIn = useCallback(
+    () =>
+      dispatch(
+        showSignInModalAction({
+          signInLinkHref: link,
+        })
+      ),
+    [link]
+  )
+
+  return registry.registryServer === NETDATA_REGISTRY_SERVER ? (
+    <Button as="a" href={link} label="SIGN IN TO CLOUD" disabled={offline} />
+  ) : (
+    <Button label="SIGN IN TO CLOUD" onClick={onSignIn} disabled={offline} />
+  )
+}
+
+export default SignIn
