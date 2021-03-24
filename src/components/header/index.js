b6ff1d2c41ce1b6346183e405f2cda9b1d010129
allelos <p.paliagkas@gmail.com>
2021-03-24 19:28:11 +0200

    Refactored top bar (header) component. - import, export, print function are now inside a dropdown next to node's name - shows number of alerts with corresponding coloring - includes the date time picker in horizontal format - has an online status badge according to browsers connectivity

---
+++ src/components/header/index.js
@@ -0,0 +1,36 @@
+import React from "react"
+import styled from "styled-components"
+import { Flex } from "@netdata/netdata-ui"
+import Node from "./node"
+import Options from "./options"
+import Version from "./version"
+import DateTimePicker from "./dateTimePicker"
+import Alarms from "./alarms"
+
+const Wrapper = styled(Flex).attrs({
+  as: "header",
+  position: "relative",
+  justifyContent: "between",
+  background: "disabled",
+  zIndex: 20,
+  width: "100%",
+  padding: [2, 0, 2, 4],
+})`
+  pointer-events: all;
+`
+
+const Header = () => (
+  <Wrapper>
+    <Flex alignItems="center" gap={3}>
+      <Node />
+      <Options />
+      <Version />
+    </Flex>
+    <Flex justifyContent="end" alignItems="center" gap={3}>
+      <DateTimePicker />
+      <Alarms />
+    </Flex>
+  </Wrapper>
+)
+
+export default Header
