bf5bcd894bef47a5d2be2920ff8686d2bac65ecc
allelos <p.paliagkas@gmail.com>
2021-03-25 10:53:31 +0200

    Fixed eslint warnings.

--- src/components/sidebar/signIn/iframe.js
+++ src/components/sidebar/signIn/iframe.js
@@ -40,6 +40,7 @@ const Iframe = ({ signedIn, setOffline }) => {
     setTimeout(() => {
       if (signInRef.current === undefined) setOffline(true)
     }, 500)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [setOffline])
 
   useEffect(() => {
@@ -66,6 +67,7 @@ const Iframe = ({ signedIn, setOffline }) => {
         payload: registryMachinesArray,
       })
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [signedIn, registry, lsValue])
 
   return <IframeContainer as="iframe" src={signInIframeUrl} onLoad={onLoad} />
