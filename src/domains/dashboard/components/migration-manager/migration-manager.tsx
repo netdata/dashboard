247ac88b73ddda647164dcadb7a351701ac8634e
Alexandros Ntitorss <a.ntitoras@gmail.com>
2022-04-11 14:50:59 +0300

    fix error with local storage that does not update when value is changing

--- src/domains/dashboard/components/migration-manager/migration-manager.tsx
+++ src/domains/dashboard/components/migration-manager/migration-manager.tsx
@@ -14,7 +14,6 @@ import { selectSignInUrl } from "domains/global/selectors"
 import { useRequestRefreshOfAccessMessage } from "hooks/use-user-node-access"
 import { selectIsCloudEnabled } from "domains/global/selectors"
 import { selectRegistry } from "domains/global/selectors"
-import { utmParametersToString } from "domains/global/selectors"
 
 // const PROMO_SIGN_UP_CLOUD: PromoProps = { userStatus: "UNKNOWN", nodeClaimedStatus: "NOT_CLAIMED" } //CLOUD
 // const PROMO_SIGN_IN_CLOUD: PromoProps = {
@@ -78,7 +77,13 @@ const MigrationManager = () => {
 
   const prefrenceID = migrationModalPromoInfo?.tickBoxOption.prefrenceID || ""
 
-  const [hasPromoSelectionSaved, savePromoRemindMeSelection] = useLocalStorage(prefrenceID)
+  /**
+   * There is seem to be a bug when we are using the useLocalStorage,
+   * the value to be returned does not change when prefrenceID is changing.
+   * For that reason we acces the localStorage directly
+   */
+  const [, savePromoRemindMeSelection] = useLocalStorage(prefrenceID)
+  const hasPromoSelectionSaved = localStorage.getItem(prefrenceID)
 
   const closeModal = () => {
     setModalOpen(false)
